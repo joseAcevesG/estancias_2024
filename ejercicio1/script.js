ab99b911cc50a6c884041f7a25f6829d1803d9da
joseCarlosAc <jose.acevesgon@gmail.com>
2024-07-10 11:16:10 -0600

    ejercicio 1

---
+++ ejercicio1/script.js
@@ -0,0 +1,11 @@
+const parrafo = document.getElementById("parrafo");
+
+const boton = document.getElementById("boton");
+
+function clickBoton() {}
+
+const parrafo2 = document.getElementById("parrafo2");
+
+function botenes(boton) {
+	let numero = Number(boton.getAttribute("numero"));
+}
