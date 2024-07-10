288b7995899beee1c1ba64ce6027f2687b1b4bb6
joseCarlosAc <jose.acevesgon@gmail.com>
2024-07-10 13:21:56 -0600

    ejercicio 2

---
+++ ejercicio2/script.js
@@ -0,0 +1,13 @@
+const padre = document.getElementById("padre");
+const padre2 = document.getElementById("padre2");
+const parrafo = document.getElementById("parrafo");
+
+let rectangulo = new Rectangulo();
+
+function crearNodos() {}
+
+function quitarNodo() {}
+
+function listaComida() {}
+
+function Rectangulo() {}
